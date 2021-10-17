1d4106949681664481ee8ead22f0c7d8b6171825
Maksim <375293629410@mail.ru>
2021-10-17 16:48:19 +0300

    add css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="generator">
+          <h2 className="generator__header">Генератор пароля</h2>
+          <div className="generator__password">
+            <h3>Пароль</h3>
+          </div>
+          <div className="wraperForm-group">
+            <div className="form-group">
+              <label htmlFor="password-strenght">Длина пароля</label>
+              <input className="range"
+                type="range"
+                id="password-strenght"
+                name="password-strenght"
+                max="20"
+                min="8" />
+            </div>
+            <button className="copy__btn">Kопировать</button>
+          </div>
+          <div className="wraperForm-group">
+            <div className="form-group">
+              <label htmlFor="uppercase-letters">Прописные буквы</label>
+              <input className="options"
+                type="checkbox"
+                id="uppercase-letters"
+                name="uppercase-letters"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="lowercase-letters">Строчные буквы</label>
+              <input className="options"
+                type="checkbox"
+                id="lowercase-letters"
+                name="lowercase-letters"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="include-numbers">Цифры</label>
+              <input className="options"
+                type="checkbox"
+                id="include-numbers"
+                name="include-numbers"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="include-symbols">Символы</label>
+              <input className="options"
+                type="checkbox"
+                id="include-symbols"
+                name="include-symbols"
+              />
+            </div>
+          </div>
+          <button className="generator__btn">Сгенерировать</button>
+        </div>
+      </div>
     </div>
   );
 }
